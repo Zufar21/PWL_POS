963a513d21f7d8bb76e8577cb69d31c5bb6c2b02
== <==>
2024-04-02 06:51:11 +0700

    js5

---
+++ resources/views/bk.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | 2141762117 PWL POS</title>
+</head>
+<body>
+    <header>
+    <h1>Selamat Datang</h1>
+        <nav>
+			<a href="/home">HOME</a>
+			|
+			<a href="/f&b">F&B</a>
+			|
+			<a href="/b&h">BEAUTY & HEALTHY</a>
+            |
+            <a href="/hc">HOME CARE</a>
+			|
+            <a href="/bk">BABY KID</a>
+	    </nav>
+
+    </header>
+
+
+
+    <br/>
+	<br/>
+	<hr/>
+	<footer>
+		<p>PWL POS - Zufar Ardian Muhaimin</p>
+	</footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+
+
+Route::get('/home', function () {
     return view('welcome');
+});
+
+
+//Route::get('/level', [LevelController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/f&b', function () {
+    return view('f&b');
+});
+
+Route::get('/b&h', function () {
+    return view('b&h');
+});
+
+Route::get('/hc', function () {
+    return view('hc');
+});
+
+Route::get('/bk', function () {
+    return view('bk');
 });
