5022881a3fbc398036619a331c446abdcddafa12
== <==>
2024-04-18 23:32:15 +0700

    Progres

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataTables\KategoriDataTable;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    //
+    public function index(KategoriDataTable $dataTable){
+        return $dataTable->render('kategori.index');
+    } 
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
+use App\Models\KategoriModel;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,6 +34,8 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::get('/f&b', function () {
     return view('f&b');
